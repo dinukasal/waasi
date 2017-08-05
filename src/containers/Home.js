91316f4658124db950cb60039303445a15c44d6c
dinuka <dinukasal@gmail.com>
2017-08-05 15:21:22 +0530

    initial commit :rocket:

---
+++ src/containers/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View, ListView
+} from 'react-native';
+
+class Home extends Component {
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+export default Home;
